fc7e5d6234b1ac09b7756774ed7fcff2b7f3583b
Luiz Fernando Ramos Lemos <kawazs@gmail.com>
2025-03-26 13:42:33 -0300

    Update run_challenge.py
    Retornando o path do cplex do mercado livre.

--- run_challenge.py
+++ run_challenge.py
@@ -4,7 +4,7 @@ import sys
 import platform
 
 # Paths to the libraries
-CPLEX_PATH = "/opt/ibm/ILOG/CPLEX_Studio2211/cplex/bin/x86-64_linux/"
+CPLEX_PATH = "$HOME/CPLEX_Studio2211/opl/bin/arm64_osx/"
 OR_TOOLS_PATH = "$HOME/Documents/or-tools/build/lib/"
 
 USE_CPLEX = True
